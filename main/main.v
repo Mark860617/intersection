63b27f7ea50a9134c7487f2487389abed1e0b86e
Ammar Akhtar <ammar.akhtar.01@gmail.com>
2017-03-29 00:48:12 -0400

    Add emergency module

--- main/main.v
+++ main/main.v
@@ -221,3 +221,12 @@ always @(posedge CLOCK_50)
 		end
 	end
 endmodule
+
+
+module emergency(SW, its_an_emergency);
+input [9:0] SW;
+output its_an_emergency;
+
+assign its_an_emergency = &SW[9:0];
+
+endmodule
