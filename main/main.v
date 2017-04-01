67c8872dcd59dafc3b371299262deb82585433dd
Patrick Harris <patricklewery.harris@mail.utoronto.ca>
2017-04-01 17:06:24 -0400

    GPIO

--- main/main.v
+++ main/main.v
@@ -1,20 +1,16 @@
-module main(CLOCK_50, SW, KEY, GPIO_0, GP_OUT, GP_IN, LEDR);
+// Top level module for the whole project
+module main(CLOCK_50, SW, KEY, GPIO_1, GPIO_0, LEDR);
 input CLOCK_50;
 input [9:0] SW;
 input [0:0] KEY;
-inout [35:0] GPIO_0;
-input [30:0] GP_OUT;
-output [4:0] GP_IN;
+input [35:0] GPIO_1;
+output [35:0] GPIO_0;
 output[9:0] LEDR;
 
 wire [2:0]led_cont;
 wire [1:0] goController;
 wire enable_next;
 
-assign GPIO_0[30:0] = GP_OUT[30:0];
-assign GP_IN[4:0] = {GPIO_0[35], 4'bzzzz};
-
-//fsm lives in another file! Make sure to include fsm.v in the quartus project
 fsm f0(
   .clk(CLOCK_50),
   .resetn(KEY[0]),
@@ -22,27 +18,18 @@ fsm f0(
   .ledOut(led_cont)
   );
 
-// The sensor input comes through port 20
-//assign goController[0] = GPIO_0[20];
-assign LEDR[0] = GP_IN[4];
-assign LEDR[1] = led_cont[0];
-assign LEDR[2] = led_cont[1];
-assign LEDR[3] = led_cont[2];
+// The sensor input comes through port 1 of GPIO_1
+assign goController[0] = GPIO_1[1] | GPIO_1[2];
+assign LEDR[0] = GPIO_1[1];
+assign LEDR[1] = GPIO_1[2];
 assign goController[1] = SW[0];
 
 led_control l0(
   .fsmIn(led_cont),
-  .greenOut(GP_OUT[1:0]),
-  .redOut(GP_OUT[3:2]),
-  .yellowOut(GP_OUT[5:4]),
-  .leftOut(GP_OUT[7:6])
+  .greenOut(GPIO_0[1:0]),
+  .redOut(GPIO_0[3:2]),
+  .yellowOut(GPIO_0[5:4]),
+  .leftOut(GPIO_0[7:6])
   );
-
-counter c0(
-	.CLOCK_50(CLOCK_50),
-	.enable(enable_next),
-  );
-
- assign LEDR[4] = enable_next;
 
 endmodule
