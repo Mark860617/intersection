d7e119345e8b253ed5736cb78771a6181853d086
Patrick Harris <patricklewery.harris@mail.utoronto.ca>
2017-04-01 19:35:11 -0400

    red light init

--- main/redlight.v
+++ main/redlight.v
@@ -6,6 +6,20 @@ wire enable;
 wire [4:0] memAdd;
 wire [3:0] inDat;
 
+
+always @ ( * ) begin
+  case (fsmState)
+    3'd0: ;
+    3'd1: ;
+    3'd2: ;
+    3'd3: ;
+    3'd4: ;
+    3'd5: ;
+    3'd6: ;
+    3'd7:;
+    default: ;
+  endcase
+end
 ram32x4 r0(
   .address(memAdd),
   .clock(clk),
